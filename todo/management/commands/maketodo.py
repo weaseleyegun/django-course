5d6631d7712ad13eed5fb2125fab5f8dbafb0b72
weaseleyegun <rlaehrjs03@gmail.com>
2025-04-07 00:39:51 +0900

    [feat] todo.html, random.html

---
+++ todo/management/commands/maketodo.py
@@ -0,0 +1,21 @@
+import sys
+from django.core.management import BaseCommand
+from todo.models import Todo
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("make todo start :)")   
+        
+        for i in range(1 ,101):
+            # todo = Todo.objects.create(name=f"테스트 todo {i}")
+            todo,created = Todo.objects.get_or_create(name=f"테스트 todo {i}")
+            if created:
+                print(f"{i}번째 todo 생성 완료")
+            else:
+                print(f"{i}번째 todo 이미 존재")
+
+        print("make todo end :)")
+        #return super().handle(*args, **options)
+        sys.stdout.write(self.style.SUCCESS("make todo end :)"))
+
--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,11 @@ from .models import Todo
 def todo_list(request):
     #return HttpResponse("Todo List")
     todos = Todo.objects.all() #select * from Todo
-    return render(request, "todo.html",{"todos": todos})
+    search = request.GET.get("search")
+    if search:
+        todos = todos.filter(name__icontains=search)
+
+    return render(request, "todo/todo.html",{"todos": todos})
 
 def todo_main(request):
     return HttpResponse("<h1>Todo Main Page</h1>")
@@ -15,10 +19,10 @@ def todo_detail(request, pk):
         todo = Todo.objects.get(id=pk)
     except Todo.DoesNotExist:
         return HttpResponse("없는 페이지 입니다.", status = 404)
-    return render(request, "todo.html", {"todo":todo})
+    return render(request, "todo/todo.html", {"todo":todo})
 
 def todo_detail_name(request, name):
-    todo = Todo.objects.filter(name__icontains=name)
-    first = todo.first()
-    last = todo.last()
-    return render(request, "todo.html", {"todo":todo, "firist":first, "last": last})
+    todos = Todo.objects.filter(name__icontains=name)
+    first = todos.first()
+    last = todos.last()
+    return render(request, "todo/todo.html", {"todos":todos, "first":first, "last": last})
